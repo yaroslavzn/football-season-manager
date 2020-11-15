79545a431672778846d56f1aead1e606dd5962df
Yaroslav Mishchenko <yaroslavzn@gmail.com>
2020-11-15 11:10:53 +0200

    add basic CRUD team operations

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -1,5 +1,9 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post } from '@nestjs/common';
 import { TeamsService } from './teams.service';
+import { CreateTeamDto } from './dto/create-team.dto';
+import { UpdateTeamDto } from './dto/update-team.dto';
+import { TeamDocument } from './entities/team.entity';
+import { Types } from 'mongoose';
 
 @Controller('teams')
 export class TeamsController {
@@ -8,17 +12,27 @@ export class TeamsController {
   ) {}
 
   @Post()
-  create(): any {}
+  async create(@Body() createTeamDto: CreateTeamDto): Promise<TeamDocument> {
+    return await this.teamsService.create(createTeamDto);
+  }
 
   @Get(':id')
-  findOne(): any {}
+  async findOne(@Param('id') id: string): Promise<TeamDocument> {
+    return await this.teamsService.findOne(id);
+  }
 
   @Get()
-  findMany(): any {}
+  async findMany(): Promise<TeamDocument[]> {
+    return await this.teamsService.findMany();
+  }
 
   @Patch(':id')
-  updateOne(): any {}
+  async updateOne(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto): Promise<TeamDocument> {
+    return await this.teamsService.update(id, updateTeamDto);
+  }
 
   @Delete(':id')
-  deleteOne(): any {}
+  async deleteOne(@Param('id') id: string): Promise<TeamDocument> {
+    return await this.teamsService.delete(id);
+  }
 }
